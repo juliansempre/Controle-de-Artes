e15ca3d3a1320cb2de3f0a4f403bf700656e5657
Julian <julian_sempre@hotmail.com>
2023-07-08 12:14:13 -0300

    com icones

--- src/main/java/com/aerocopias/controledeartes/HelloApplication.java
+++ src/main/java/com/aerocopias/controledeartes/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
 
 import java.io.IOException;
 
@@ -12,7 +13,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 950, 700);
-        stage.setTitle("Hello!");
+        stage.setTitle("Aero Cópias");
+        Image icon = new Image(getClass().getResourceAsStream("/img/logo.png"));
+        stage.getIcons().add(icon);
         stage.setScene(scene);
         stage.show();
     }
